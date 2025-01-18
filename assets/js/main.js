40341d4cccf2d5bde572b1891922f1270647336e
VoTk4N <dikiaronzolo-95@live.it>
2025-01-18 19:13:43 +0100

    sisitemata indentazione

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,3 @@
-
 //ELEMENTI
 const header = document.createElement('header');
 const logo = document.createElement('logo');
@@ -8,7 +7,6 @@ const display = document.createElement('div');
 const container2 = document.createElement('div');
 const btnplus = document.createElement('button');
 const btnless = document.createElement('button');
-
 
 //CLASSI
 header.classList.add('header');
@@ -22,7 +20,6 @@ btnless.classList.add('less');
 btnreset.classList.add('btn');
 btnreset.classList.add('reset');
 
-
 document.body.insertBefore(header, document.body.firstChild);
 header.appendChild(logo);
 logo.textContent = 'Counter';
@@ -30,9 +27,7 @@ logo.textContent = 'Counter';
 header.appendChild(btnreset);
 btnreset.textContent = 'Reset';
 
-
 header.insertAdjacentElement('afterend', container);
-
 
 container.appendChild(display);
 display.textContent = '0';
@@ -43,101 +38,68 @@ container2.appendChild(btnplus);
 btnplus.textContent = '+';
 btnplus.setAttribute('title', 'Premi (P)');
 
-
 container2.appendChild(btnless);
 btnless.textContent = '-';
 btnless.setAttribute('title', 'Premi (L)');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //COUNTER
+let score = JSON.parse(localStorage.getItem('score')) || {count: 0};
 
+document.querySelector('.display').innerHTML = score.count;
+btnplus.addEventListener('click', counterPlus);
+btnless.addEventListener('click', counterLess);
+btnreset.addEventListener('click', counterReset);
 
-let score = JSON.parse(localStorage.getItem('score')) || { 
-  count: 0};
-  document.querySelector('.display').innerHTML = score.count;
-  btnplus.addEventListener('click', counterPlus);
+let currentAction = null;
+display.innerHTML = score.count;
+let interval;
 
-  btnless.addEventListener('click', counterLess);
-  btnreset.addEventListener('click', counterReset);
-
-  let currentAction = null;
- display.innerHTML = score.count;
- let interval;
-
-
- function counterPlus() { 
+function counterPlus() { 
   score.count++; 
   document.documentElement.style.setProperty('--color-default--', '#ffa500');
 
   localStorage.setItem('score', JSON.stringify(score));
-   display.innerHTML = score.count; 
-  }
+  display.innerHTML = score.count; 
+}
    
-   function counterLess() { if (score.count > 0) { 
-    score.count--;
-    document.documentElement.style.setProperty('--color-default--', '#0696bb');
-     localStorage.setItem('score', JSON.stringify(score));
-      display.innerHTML = score.count; } 
-
-    }
+function counterLess() { if (score.count > 0) { 
+  score.count--;
+  document.documentElement.style.setProperty('--color-default--', '#0696bb');
+  localStorage.setItem('score', JSON.stringify(score));
+  display.innerHTML = score.count; } 
+}
    
-   function startCounter(action) { 
-    currentAction = action;
-     interval = setInterval(() => { if (currentAction === 'plus') { 
+function startCounter(action) { 
+  currentAction = action;
+  interval = setInterval(() => {      
+    if (currentAction === 'plus') { 
       counterPlus(); } 
-      else if (currentAction === 'less') { 
-        counterLess(); } }, 300);
-       }
-
-
+    else if (currentAction === 'less') { 
+      counterLess(); } }, 300);
+}
 
 
 function stopCounter() { 
   clearInterval(interval); 
-  currentAction = null; }
+  currentAction = null; 
+}
 
-  function returnYellow(){
-    setTimeout(() => {
-      document.documentElement.style.setProperty('--color-default--', '#ffff00');
-
+function returnYellow(){
+  setTimeout(() => {
+  document.documentElement.style.setProperty('--color-default--', '#ffff00');
   }, 300);
-  }
+}
 
-  document.addEventListener('keyup', returnYellow );
+document.addEventListener('keyup', returnYellow );
 
-  btnplus.addEventListener('mouseleave', returnYellow); 
-  btnplus.addEventListener('mouseup', returnYellow); 
-  btnplus.addEventListener('touchend', returnYellow);
+btnplus.addEventListener('mouseleave', returnYellow); 
+btnplus.addEventListener('mouseup', returnYellow); 
+btnplus.addEventListener('touchend', returnYellow);
  
 
-  btnless.addEventListener('mouseleave', returnYellow); 
-  btnless.addEventListener('mouseup', returnYellow); 
-  btnless.addEventListener('touchend', returnYellow); 
-
-
-
-
+btnless.addEventListener('mouseleave', returnYellow); 
+btnless.addEventListener('mouseup', returnYellow); 
+btnless.addEventListener('touchend', returnYellow); 
 
 btnplus.addEventListener('mousedown', () => startCounter('plus')); 
 btnplus.addEventListener('mouseup', stopCounter); 
@@ -155,13 +117,14 @@ btnless.addEventListener('touchend', stopCounter);
 
 document.addEventListener('keydown', function(event) { 
   if (event.key === 'p' || event.key === 'P') {  
-   counterPlus();
+    counterPlus();
    } 
- });
- document.addEventListener('keydown', function(event) { 
+});
+  
+document.addEventListener('keydown', function(event) { 
    if (event.key === 'l' || event.key === 'L') {  
     counterLess();
-  }});
+}});
 
 
 function counterReset() {
@@ -172,17 +135,16 @@ function counterReset() {
   // Utilizza setTimeout per dare tempo al browser di applicare lo stile
   setTimeout(() => {
       // Chiedi conferma all'utente
-      const userResponse = confirm('Vuoi procedere con il reset?');
+    const userResponse = confirm('Vuoi procedere con il reset?');
       
-      if (userResponse) {
+    if (userResponse) {
           // Resetta il punteggio
           score.count = 0;
           localStorage.removeItem('score');
           document.querySelector('.display').innerHTML = score.count;
-      }
-
+    }
       // Cambia il colore di default a giallo dopo che l'utente ha risposto
-      document.documentElement.style.setProperty('--color-default--', 'yellow');
+    document.documentElement.style.setProperty('--color-default--', 'yellow');
   }, 0);
 }
 
